2882059d577059a0c049c34be79e70a46c71d761
Taras Mankovski <taras@quandl.com>
2014-09-10 06:25:02 -0400

    Initial commit

--- index.js
+++ index.js
@@ -1,3 +1,39 @@
+'use strict';
+
+var path = require('path');
+var fs   = require('fs');
+
 module.exports = {
-  name: 'ember-cli-velocity'
+  name: 'ember-cli-velocity',
+  treeFor: function treeFor(name) {
+    if (name !== 'vendor') { return; }
+
+    var treePath = path.join('node_modules', 'ember-cli-velocity', 'node_modules');
+
+    var addon;
+
+    if (fs.existsSync(treePath)) {
+      addon = unwatchedTree(treePath);
+    }
+
+    return addon;
+  },
+  included: function included(app) {
+    this.app = app;
+    var options = this.app.options.velocityOptions || {enabled: true, ui: false};
+
+    if (options.enabled) {
+      this.app.import('vendor/velocity-animate/velocity.js');
+    }
+    if (options.ui) {
+      this.app.import('vendor/velocity-animate/velocity.ui.js');
+    }
+  }
 };
+
+function unwatchedTree(dir) {
+  return {
+    read:    function() { return dir; },
+    cleanup: function() { }
+  };
+}
