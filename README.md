23593ea9073257f3a5ccc4edca7838ac2c134e4f
Taras Mankovski <tarasm@gmail.com>
2015-02-08 11:21:23 -0500

    Added better documentation

--- README.md
+++ README.md
@@ -1,10 +1,25 @@
 # ember-cli-velocity [![Build Status](https://travis-ci.org/taras/ember-cli-velocity.svg)](https://travis-ci.org/taras/ember-cli-velocity)
 
-Adds Velocity.js to application's vendor directory and imports Velocity for use in application.
+Adds Velocity.js to application's vendor directory and imports Velocity for use in application. 
+This addon shims Promise with Ember.RSVP.Promise if the browser doesn't have native Promise support.
 
 ## Installation: 
 
 `npm install --save ember-cli-velocity`
+
+## Configuration
+
+You can activate UI pack in your project's `Brocfile.js`.
+
+```
+var app = new EmberApp({
+  velocityOptions: {
+    enabled: true, # enabled the addon (default: true)
+    ui: false # add Velocity UI Pack (default: false)
+  }
+});
+return app;
+```
 
 ## Development
 
--- index.js
+++ index.js
@@ -6,23 +6,25 @@ module.exports = {
   name: 'ember-cli-velocity',
   treeForVendor: function treeForVendor() {
     var unwatchedTree = require('broccoli-unwatched-tree');
-    var js = this.pickFiles(unwatchedTree(path.join(__dirname, 'node_modules', 'velocity-animate')), {
+    return this.pickFiles(unwatchedTree(path.join(__dirname, 'node_modules', 'velocity-animate')), {
       srcDir: '/',
       files: ['*.js'],
       destDir: 'velocity-animate'
     });
-    return js;
   },
   included: function included(app) {
     this.app = app;
     var options = this.app.options.velocityOptions || {enabled: true, ui: false};
 
-    if (options.enabled) {
-      this.app.import('vendor/velocity-animate/velocity.js');
+    if (options.enabled == false) {
+      return;
     }
+
+    this.app.import('vendor/velocity-animate/velocity.js');
     if (options.ui) {
       this.app.import('vendor/velocity-animate/velocity.ui.js');
     }
     this.app.import('vendor/ember-cli-velocity/velocity-promise-shim.js');
+
   }
 };
