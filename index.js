037e6a170e8804b30ca566039a56fb63e674d08a
Chris Thoburn <runspired@gmail.com>
2015-12-25 04:31:42 -0600

    guard against app.import errors

--- index.js
+++ index.js
@@ -2,29 +2,35 @@
 /* global process, require, module, __dirname */
 var path = require('path');
 
+var unwatchedTree = require('broccoli-unwatched-tree');
+var mergeTrees = require('broccoli-merge-trees');
+var pickFiles = require('broccoli-static-compiler');
+
 module.exports = {
   name: 'ember-cli-velocity',
 
   treeForVendor: function treeForVendor() {
-    var unwatchedTree = require('broccoli-unwatched-tree');
-    var mergeTrees = require('broccoli-merge-trees');
-    var pickFiles = require('broccoli-static-compiler');
-    var emberCliVelocity = unwatchedTree(path.join(__dirname, 'vendor'));
-    var velocityAnimate = pickFiles(unwatchedTree(require.resolve('velocity-animate').replace('velocity.js', '')), {
-      srcDir: '/',
-      files: ['*.js'],
-      destDir: 'velocity-animate'
+     var emberCliVelocity = unwatchedTree(path.join(__dirname, 'vendor'));
+     var velocityAnimate = pickFiles(unwatchedTree(require.resolve('velocity-animate').replace('velocity.js', '')), {
+       srcDir: '/',
+       files: ['*.js'],
+       destDir: 'velocity-animate'
     });
     return mergeTrees([emberCliVelocity, velocityAnimate]);
   },
 
   velocityOptions: function () {
     var env  = process.env.EMBER_ENV;
-    return this.project.config(env).velocityOptions || {enabled: true, ui: false};
+    return this.project.config(env).velocityOptions || { enabled: true, ui: false };
   },
 
-  included: function included(app) {
+  included: function(app) {
     this._super.included.apply(this, arguments);
+
+    // see: https://github.com/ember-cli/ember-cli/issues/3718
+    if (typeof app.import !== 'function' && app.app) {
+      app = app.app;
+    }
 
     var emberCLIVersion = app.project.emberCLIVersion();
     if (emberCLIVersion < '0.0.41') {
